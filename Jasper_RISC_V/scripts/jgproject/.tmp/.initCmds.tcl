fed067ec24c29530bae045edd8c7874f7eddea74
aleksa <djokovic64@gmail.com>
2025-01-26 12:53:39 +0100

    Update readme

--- Jasper_RISC_V/scripts/jgproject/.tmp/.initCmds.tcl
+++
@@ -1,3 +0,0 @@
-@puts {INFO: successfully checked out licenses "jasper_interactive" and "jasper_papp".}
-@puts {INFO: reading configuration file "/home/aleksadj/.config/cadence/jasper.conf".}
-@get_message -clear
---
+++ README.md
@@ -0,0 +1,2 @@
+NEROV32 RISC-V core verification on CPU level.
+Application of SST method.
